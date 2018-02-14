cc5e167c8dd5a91dce98c8a60e1346f47e79b99b
Nick Fraenkel <nickfraenkel@gmail.com>
2018-02-14 20:29:39 +0000

    Create setup-new-machine.sh script for setting up a new system
    Initially, this script simply makes sure the latest stable version of Git
    (from the official repo) is installed.

---
+++ setup-new-machine.sh
@@ -0,0 +1,11 @@
+# This script will set up a new system, installing a bunch of useful tools
+# It only needs to be run once (and, depending on your needs, possibly not
+# even once). You'll probably need to `sudo` it: ie. `sudo bash setup.sh`
+#
+# It was written for use on Windows Subsystem for Linux (WSL), so mileage
+# may vary on other OSes.
+
+apt-add-repository ppa:git-core/ppa
+apt-get update
+
+apt-get install git
