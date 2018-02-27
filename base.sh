f17c42325e213ccf89076c46df92397a822c05bd
Nick Fraenkel <nickfraenkel@gmail.com>
2018-02-27 21:30:33 +0000

    Add optional gitignored '.extra' file for local settings
    This is a pattern copied from Paul Irish's dotfiles. `.extra` is a useful
    place to store aliases, config settings, etc, which are only for the local
    machine.

--- base.sh
+++ base.sh
@@ -4,6 +4,10 @@ source ~/.dotfiles/completion/git-prompt.sh
 source ~/.dotfiles/.aliases
 source ~/.dotfiles/.bash_prompt
 
+# .extra is an optional .gitignored file for local settings:
+[ -r ~/.dotfiles/.extra ] && source ~/.dotfiles/.extra
+
+# Like .extra, private local scripts can go in .dotfiles/private:
 if test -e `echo "~/.dotfiles/private/*.sh" | cut -d' ' -f1`
 then
   for file in ~/.dotfiles/private/*.sh
