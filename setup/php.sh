e8ecb65078bee29dab4431892988e8fb14aa4a83
Nick Fraenkel <nickfraenkel@gmail.com>
2018-03-13 20:13:15 +0000

    Add extra PHP packages for Craft CMS to PHP setup script
    The previous version of this script lacked a few packages that are
    now either recommended or required for Craft CMS. This commit adds
    them. It also corrects a comment that incorrectly mentioned Composer,
    since Composer now has its own, separate setup script.

--- setup/php.sh
+++ setup/php.sh
@@ -1,12 +1,16 @@
-# Install PHP and Composer:
+# Install PHP:
 # (Includes PHP extensions required by Craft CMS)
 PHP_VERSION=7.1
 sudo apt-add-repository ppa:ondrej/php
 sudo apt-get update
 
+sudo apt-get -y install imagemagick
+sudo apt-get -y install php-imagick
 sudo apt-get -y install php${PHP_VERSION}
 sudo apt-get -y install php${PHP_VERSION}-curl
+sudo apt-get -y install php${PHP_VERSION}-intl
 sudo apt-get -y install php${PHP_VERSION}-mbstring
+sudo apt-get -y install php${PHP_VERSION}-mysql
 sudo apt-get -y install php${PHP_VERSION}-xml
 sudo apt-get -y install php${PHP_VERSION}-zip
 
