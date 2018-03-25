219ade67cb2c849b330efa1508ca3ee6697f5d40
Nick Fraenkel <nickfraenkel@gmail.com>
2018-03-25 19:06:20 +0100

    Remove conditional from yarn setup script
    Previously this conditional meant the script would not do anything if a
    version of yarn was already present. However, this isn't necessarily
    desirable: this script will install the latest version of yarn, and even
    a long out-of-date version would prevent it running. This commit removes
    the conditional so that it can be run even if yarn is present.

--- setup/yarn.sh
+++ setup/yarn.sh
@@ -1,6 +1,4 @@
 # Install yarn:
-if ! type yarn >/dev/null 2>&1; then
-  curl -o- -L https://yarnpkg.com/install.sh | bash
-  source ~/.bashrc
-fi
+curl -o- -L https://yarnpkg.com/install.sh | bash
+source ~/.bashrc
 
