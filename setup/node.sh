1cf274f4d806739274506a96feea95823e8670c4
Nick Fraenkel <nickfraenkel@gmail.com>
2018-12-18 14:49:00 +0000

    Separate nvm install script from Node install script
    Moving the nvm install steps into a separate script, and making the
    install step always run should make it easier to update nvm (since you
    can just bump the version number and then run the nvm script) without
    also having to update Node.
    
    We should also reload `.bashrc` after updating nvm, to ensure we're
    using the most up-to-date version.

--- setup/node.sh
+++ setup/node.sh
@@ -1,11 +1,7 @@
 # Install nvm (Node Version Manager):
 # (nb. this might require some tweaking in order to allow it to work ok with sudo)
 if ! type nvm >/dev/null 2>&1; then
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  source ~/.dotfiles/setup/nvm.sh
 fi;
 
 # Install the latest LTS version of Node:
