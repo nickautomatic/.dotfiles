2a9f13a5092362c985522197cba6f6cfd1889496
Nick Fraenkel <nickfraenkel@gmail.com>
2019-01-07 16:39:58 +0000

    Fix error in yarn install process
    There was a missing space in this command for some reason.

--- setup/yarn.sh
+++ setup/yarn.sh
@@ -1,5 +1,5 @@
 # Install yarn:
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee/etc/apt/sources.list.d/yarn.list
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get install yarn
 
