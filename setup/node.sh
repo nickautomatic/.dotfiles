8d855e445685388d2563dead5d164d9086a33716
Nick Fraenkel <nickfraenkel@gmail.com>
2020-05-16 18:58:56 +0100

    Use "n" instead of "nvm" for managing NodeJS version
    `n` is an alternative Node version manager which provides much the same
    functionality as `nvm`. However, `nvm` causes a significant increase to
    bash's start-up time. Switching to `n` allows new terminal tabs to open
    much faster.

--- setup/node.sh
+++ setup/node.sh
@@ -1,15 +1,11 @@
-# Install nvm (Node Version Manager):
-# (nb. this might require some tweaking in order to allow it to work ok with sudo)
-if ! type nvm >/dev/null 2>&1; then
-  source ~/.dotfiles/setup/nvm.sh
+# Install n (Node version manager):
+if ! type n >/dev/null 2>&1; then
+  curl -L https://git.io/n-install | bash
 fi;
 
 # Install the latest LTS version of Node:
-nvm install --lts
-
-# Make sure nvm uses the latest available node version by default:
-nvm alias default node
+n lts
 
 # Use it (without having to open a new terminal):
-nvm use node
+source ~/.bashrc
 
