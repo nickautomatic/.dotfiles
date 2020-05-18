c731516bcd55051e5120f27a6bf5fa01131820e7
Nick Fraenkel <nickfraenkel@gmail.com>
2020-05-18 13:38:26 +0100

    Fix issue with Node install script
    These two commands were in the wrong order: after installing `n` we need
    to source .bashrc *before* trying to use `n`

--- setup/node.sh
+++ setup/node.sh
@@ -3,9 +3,9 @@ if ! type n >/dev/null 2>&1; then
   curl -L https://git.io/n-install | bash
 fi;
 
-# Install the latest LTS version of Node:
-n lts
-
 # Use it (without having to open a new terminal):
 source ~/.bashrc
+
+# Install the latest LTS version of Node:
+n lts
 
