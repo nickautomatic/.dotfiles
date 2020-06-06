7843530f541915e71153010cbaadcb380728cec4
Nick Fraenkel <nickfraenkel@gmail.com>
2020-06-06 19:56:34 +0100

    Prepend n to PATH
    When n is installed, it appends its directory to the PATH. This can
    present a problem with WSL, because WSL also (by default, anyway)
    imports the PATH variable from Windows. Which means that if Node is
    installed in Windows, it takes precedence over the version installed
    via `n`
    
    Prepending n to the PATH in WSL ensures that the version of Node
    installed via `n` takes precedence.
    
    I haven't attempted to remove the line that `n-install` adds, so as a
    result of this this line does now get both prepended and appended.
    I don't think that should really cause any problems though.

--- setup/node.sh
+++ setup/node.sh
@@ -3,6 +3,11 @@ if ! type n >/dev/null 2>&1; then
   curl -L https://git.io/n-install | bash
 fi;
 
+# Prepend n to PATH:
+if (! grep 'export PATH="$N_PREFIX/bin:$PATH"' ~/.bashrc) >/dev/null; then
+  echo 'export PATH="$N_PREFIX/bin:$PATH"' >> ~/.bashrc;
+fi;
+
 # Use it (without having to open a new terminal):
 source ~/.bashrc
 
