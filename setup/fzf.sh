342e53d35c7075ff98d87d743eaec696a2d76a85
Nick Fraenkel <nickfraenkel@gmail.com>
2020-06-13 03:35:52 +0100

    Use ripgrep for default fzf command
    ripgrep is very, very fast, and this also ignores the contents of the
    .git directory, so using it for fzf's default command (on, eg. typing
    `**` and hitting Tab) is nice and quick.
    
    Let's only add this if ripgrep is available (it's harder to install on
    some systems - eg. Ubuntu < 18)
    
    This is copied from here [1].
    
    [1] https://github.com/nickjj/dotfiles/blob/master/.bashrc

--- setup/fzf.sh
+++ setup/fzf.sh
@@ -4,5 +4,10 @@ if ! type fzf >/dev/null 2>&1; then
   ~/.fzf/install
 fi
 
+# Use ripgrep for default fzf command:
+if (type rg && ! grep 'FZF_DEFAULT_COMMAND' ~/.bashrc) >/dev/null 2>&1; then
+  echo "export FZF_DEFAULT_COMMAND=\"rg --files --hidden --follow --glob '!.git'\"" >> ~/.bashrc
+fi
+
 # Enable fzf in the current terminal:
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
