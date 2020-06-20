c540085c4d8f5d67896d5d45d448ab1ac6c0c0a2
Nick Fraenkel <nickfraenkel@gmail.com>
2020-06-20 19:35:38 +0100

    Add gh (Github CLI tool) setup script
    gh is a handy tool for interacting with PRs, issues, etc from the
    command line.

---
+++ setup/gh.sh
@@ -0,0 +1,6 @@
+# Install gh (Github CLI tool)
+GH_VERSION=0.10.0
+curl -L -O https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.deb
+sudo dpkg -i gh_${GH_VERSION}_linux_amd64.deb
+rm gh_${GH_VERSION}_linux_amd64.deb
+
