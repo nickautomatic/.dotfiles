63ca898c4df295f61f8505b8ac97f5abf60a2d02
Nick Fraenkel <nickfraenkel@gmail.com>
2020-07-01 00:51:18 +0100

    Create Ansible playbook and bootstrap script
    This commit introduces a bootstrap script that will install Ansible, if
    necessary, and then run the playbook in `ansible/playbook.yml`
    
    For now the playbook just uses `apt` to install some useful tools.
    
    In subsequent commits, I'll move more of the setup scripts into Ansible,
    and remove them from `setup`. I might leave some of the more optional
    things in `setup` for now if I don't want them to always be installed.
    
    Why use Ansible?
    
    The main reason is that it's idempotent: ie. it can be run multiple
    times and will always leave things in the same state. So if something's
    already installed running the installer again won't mess with the
    existing installation.
    
    I've tried to write the bash setup scripts in a moderately idempotent
    way (checking for the presence of things before performing certain
    actions, etc), but it's hard to be sure I've always got this right.
    
    Using Ansible makes it much easier to configure things in a declarative
    way: I want this and this and this installed, but I don't necessarily
    need to specify how (or whether) to install them. Ansible will handle
    that for me.

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -22,9 +22,6 @@ prompt () {
   done
 }
 
-# Install build essentials:
-source ${SETUP_DIR}/build-essential.sh;
-
 # Install Git:
 if prompt "Install Git? [y/n]"; then source ${SETUP_DIR}/git.sh; fi
 
@@ -66,15 +63,6 @@ if prompt "Install Vagrant? [y/n]"; then source setup/vagrant.sh; fi
 
 # Install some helpful tools:
 if prompt "Install some helpful command line tools (fzf, etc)? [y/n]"; then
-  # Install ag (silver searcher):
-  sudo apt-get install silversearcher-ag
-
-  # Install ripgrep:
-  sudo apt-get install ripgrep
-
-  # Install fd (fast alternative to find):
-  sudo apt install fd-find
-
   # Install fzf (fuzzy finder):
   source ${SETUP_DIR}/fzf.sh
 
@@ -84,18 +72,6 @@ if prompt "Install some helpful command line tools (fzf, etc)? [y/n]"; then
   # Nice git diffs:
   npm install -g diff-so-fancy 
   git config --global core.pager "diff-so-fancy | less --tabs=1,5 -R"
-
-  # htop - an interactive process viewer for Unix
-  sudo apt-get install htop
-
-  # httpie - a more user-friendly alternative to curl
-  sudo apt-get install httpie
-
-  # lynx - a text browser for the World Wide Web
-  sudo apt-get install lynx
-
-  # tig - a handy UI for git:
-  sudo apt-get install tig
 
   # Simplified man pages:
   npm install -g tldr
