6ddcd8dfc4359249864fe7bb993f977268b25b7f
Nick Fraenkel <nickfraenkel@gmail.com>
2020-07-01 18:44:32 +0100

    Use Ansible to symlink dotfiles
    Rather than do this in a bash script, let's use Ansible to make sure
    the relevant dotfiles are symlinked to from the home directory.

--- install.sh
+++ install.sh
@@ -5,10 +5,3 @@ fi;
 
 # Include dotfiles .gitconfig:
 git config --global include.path ~/.dotfiles/.gitconfig
-
-# Symlink some things:
-[ ! -e ~/.gemrc ] && ln -s ~/.dotfiles/.gemrc ~/.gemrc
-[ ! -e ~/.tigrc ] && ln -s ~/.dotfiles/.tigrc ~/.tigrc
-[ ! -e ~/.vimrc ] && ln -s ~/.dotfiles/.vimrc ~/.vimrc
-[ ! -e ~/.tmux.conf ] && ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
