c6730ad70d0e5b9ef0473782893f086b19b353ed
Nick Fraenkel <nickfraenkel@gmail.com>
2020-07-02 19:44:53 +0100

    Bump gh version to 0.10.1

--- setup/gh.sh
+++ setup/gh.sh
@@ -1,5 +1,5 @@
 # Install gh (Github CLI tool)
-GH_VERSION=0.10.0
+GH_VERSION=0.10.1
 curl -L -O https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.deb
 sudo dpkg -i gh_${GH_VERSION}_linux_amd64.deb
 rm gh_${GH_VERSION}_linux_amd64.deb
