afc38ae54e68a5ff025ab065d7cb5ab39c9c93d1
Nick Fraenkel <nickfraenkel@gmail.com>
2020-07-11 19:30:18 +0100

    Install gh via Ansible

--- setup/gh.sh
+++
@@ -1,6 +0,0 @@
-# Install gh (Github CLI tool)
-GH_VERSION=0.10.1
-curl -L -O https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.deb
-sudo dpkg -i gh_${GH_VERSION}_linux_amd64.deb
-rm gh_${GH_VERSION}_linux_amd64.deb
-
