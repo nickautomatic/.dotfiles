8228b0433b92b19eedf988a70bab2cb90a75fdd2
Nick Fraenkel <nickfraenkel@gmail.com>
2020-10-20 13:39:59 +0100

    Remove homebrew setup script
    At some point I tried Homebrew, thinking it might make it easier to
    follow MacOS-oriented install steps, but I ran into various problems
    and found it generally less reliable than `apt`. It's been ages since
    I used it and I no longer have it installed locally. If I were ever going
    to try it again I'd start from scratch, so this setup script can go.

--- setup/homebrew.sh
+++
@@ -1,7 +0,0 @@
-# Install Homebrew:
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-if ! type brew >/dev/null 2>&1; then
-  echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
-  source .bashrc
-fi
