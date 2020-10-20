f905aafa39f17d782091df637d615398bea2d9e0
Nick Fraenkel <nickfraenkel@gmail.com>
2020-10-20 13:17:52 +0100

    Set Git's include.path via Ansible
    Previously this was done in `bootstrap.sh`, but there's no particular
    reason to do it there. It seems tidier to set it via Ansible, alongside
    the other git tasks.
    
    Note that using Ansible's `{{ home }}` variable rather than `~` is needed
    to make this task idempotent: when I set the value to
    `~/.dotfiles/.gitconfig` the task was always marked "changed". I think
    this was because the `~` was being substituted for the full path to the
    home directory. Using `{{ home }}` instead seems to avoid this and
    preserves idempotence: the first run is marked "changed", and subsequent
    runs are not.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,7 @@ if ! grep -Fq 'source ~/.dotfiles/.bashrc' ~/.bashrc; then
   echo 'source ~/.dotfiles/.bashrc' >> ~/.bashrc
 fi;
 
-# Include dotfiles .gitconfig:
-git config --global include.path ~/.dotfiles/.gitconfig
-
+# Install Ansible (on first run):
 if [[ ! -x `which ansible` ]]; then
   echo "Installing Ansible"
   sudo apt update
